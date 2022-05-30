5abc7020412936f7f5e80cf1d9eab2a549a1d874
jahangir <jahangir2km@gmail.com>
2022-05-30 15:51:12 +0600

    first commit

---
+++ src/components/Shared/Headers/Headers.js
@@ -0,0 +1,14 @@
+/** @format */
+
+import React from "react";
+import "./Headers.css";
+
+const Headers = () => {
+  return (
+    <div>
+      <h2>This is Header</h2>
+    </div>
+  );
+};
+
+export default Headers;
