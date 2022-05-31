b1162a554b6fdf7de664e1169595872dff5775da
Noman_React_developer <jahangir2km@gmail.com>
2022-05-31 17:49:01 +0600

    ujpdated

--- src/components/Home/DataScienceCourses/DataScienceCourses.js
+++ src/components/Home/DataScienceCourses/DataScienceCourses.js
@@ -5,6 +5,7 @@ import image1 from '../../../images/Trending-courses/course_1.png';
 import image2 from '../../../images/Trending-courses/course_2.jpg';
 import image3 from '../../../images/Trending-courses/course_3.jpg';
 
+
 const DataScienceCourses = () => {
     return (
         <div className="container mt-5 p-5 trending_courses text-start">
@@ -14,7 +15,7 @@ const DataScienceCourses = () => {
            <div className="card p-3 rounded-0 h-100">
            <img src={image1} className="card-img-top rounded-0" alt="..."/>
            <button type="button" className="btn-light mt-3 w-25 text-muted">Flutter</button>
-  
+
            <div className="card-body pt-4 pb-4">
           <h4 className="card-title">Data Science Basics</h4>
           <h6> <span className="dollar_text  pe-2">$250.00</span> 7 days session</h6>
--- src/components/Home/JoinCommunity/JoinCommunity.js
+++ src/components/Home/JoinCommunity/JoinCommunity.js
@@ -1,16 +1,40 @@
-import React from 'react';
-import './JoinCommunity.css';
+import React from "react";
+import "./JoinCommunity.css";
 
 const JoinCommunity = () => {
-    return (
-    <div class="container partner pt-5 mt-5">
-        <div class="row">
-          <div class="col-sm-8">col-sm-8</div>
-          <div class="col-sm-4">col-sm-4</div>
-        </div>
-    </div>
-    );
-};
+  return (
 
+    // <div class="container partner pt-5 mt-5">
+    //     <div class="row">
+    //       <div class="col-sm-8">col-sm-8</div>
+    //       <div class="col-sm-4">col-sm-4</div>
+    //     </div>
+    // </div>
+
+    
+    <div className="container">
+      <div class="card mb-3 join_community">
+        <div class="row g-0">
+          <div class="col-md-2">
+            <i className="fa fa-train fs-1 ps-3" aria-hidden="true"></i>
+          </div>
+          <div class="col-md-6">
+            <div class="card-body text-start">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit 
+              </p>
+              <p class="card-text">
+                <small class="text-muted">Last updated 3 mins ago</small>
+              </p>
+            </div>
+          </div>
+          <div class="col-md-6"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default JoinCommunity;
